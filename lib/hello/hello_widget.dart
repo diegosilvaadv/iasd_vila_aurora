6bab75b132e3ca8c30edd39cf0913f764b283188
diegosilvaadv <91504617+diegosilvaadv@users.noreply.github.com>
2021-11-01 21:29:03 -0300

    Create hello_widget.dart

---
+++ lib/hello/hello_widget.dart
@@ -0,0 +1,31 @@
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HelloWidget extends StatefulWidget {
+  HelloWidget({Key key}) : super(key: key);
+
+  @override
+  _HelloWidgetState createState() => _HelloWidgetState();
+}
+
+class _HelloWidgetState extends State<HelloWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: FlutterFlowTheme.primaryColor,
+        automaticallyImplyLeading: true,
+        actions: [],
+        centerTitle: true,
+        elevation: 4,
+      ),
+      backgroundColor: Color(0xFFF5F5F5),
+    );
+  }
+}
