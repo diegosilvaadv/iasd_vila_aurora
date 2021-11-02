91001ba4a2f0f76af8424b1a016238c1bc172dab
diegosilvaadv <91504617+diegosilvaadv@users.noreply.github.com>
2021-11-01 21:31:06 -0300

    Update hello_widget.dart

--- lib/hello/hello_widget.dart
+++ lib/hello/hello_widget.dart
@@ -26,6 +26,31 @@ class _HelloWidgetState extends State<HelloWidget> {
         elevation: 4,
       ),
       backgroundColor: Color(0xFFF5F5F5),
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: FlutterFlowTheme.secondaryColor,
+                ),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                  child: Text(
+                    'Hello World',
+                    textAlign: TextAlign.center,
+                    style: FlutterFlowTheme.title1,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
