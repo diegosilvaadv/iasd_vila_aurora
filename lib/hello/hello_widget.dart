798e5f1e1cca10ea19e282de4116b9a2d4ad76d6
diegosilvaadv <91504617+diegosilvaadv@users.noreply.github.com>
2021-11-01 21:36:00 -0300

    Update hello_widget.dart

--- lib/hello/hello_widget.dart
+++ lib/hello/hello_widget.dart
@@ -1,3 +1,4 @@
+import '../backend/backend.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
@@ -21,34 +22,59 @@ class _HelloWidgetState extends State<HelloWidget> {
       appBar: AppBar(
         backgroundColor: FlutterFlowTheme.primaryColor,
         automaticallyImplyLeading: true,
+        title: Text(
+          'teste dados',
+          style: FlutterFlowTheme.bodyText1,
+        ),
         actions: [],
         centerTitle: true,
         elevation: 4,
       ),
       backgroundColor: Color(0xFFF5F5F5),
       body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: FlutterFlowTheme.secondaryColor,
-                ),
-                child: Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
-                  child: Text(
-                    'Hello World',
-                    textAlign: TextAlign.center,
-                    style: FlutterFlowTheme.title1,
+        child: StreamBuilder<List<HelloRecord>>(
+          stream: queryHelloRecord(),
+          builder: (context, snapshot) {
+            // Customize what your widget looks like when it's loading.
+            if (!snapshot.hasData) {
+              return Center(
+                child: SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: SpinKitThreeBounce(
+                    color: FlutterFlowTheme.secondaryColor,
+                    size: 50,
                   ),
                 ),
-              ),
-            )
-          ],
+              );
+            }
+            List<HelloRecord> columnHelloRecordList = snapshot.data;
+            return Column(
+              mainAxisSize: MainAxisSize.max,
+              children:
+                  List.generate(columnHelloRecordList.length, (columnIndex) {
+                final columnHelloRecord = columnHelloRecordList[columnIndex];
+                return Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: FlutterFlowTheme.secondaryColor,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                      child: Text(
+                        columnHelloRecord.nome,
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.title1,
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            );
+          },
         ),
       ),
     );
