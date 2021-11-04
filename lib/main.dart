358671185ae89025d6a8154adb618fac39a2798d
diegosilvaadv <91504617+diegosilvaadv@users.noreply.github.com>
2021-11-03 21:33:37 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,16 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+//Remove this method to stop OneSignal Debugging 
+OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+OneSignal.shared.setAppId("YOUR_ONESIGNAL_APP_ID");
+
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+});
+
 class NavBarPage extends StatefulWidget {
   NavBarPage({Key key, this.initialPage}) : super(key: key);
 
