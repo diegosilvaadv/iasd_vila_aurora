c711794564fcf7c54ef6d0a04ff804b21184d678
diegosilvaadv <91504617+diegosilvaadv@users.noreply.github.com>
2021-11-04 22:34:57 -0300

    Create view_events_widget.dart

---
+++ lib/components/view_events_widget.dart
@@ -0,0 +1,40 @@
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ViewEventsWidget extends StatefulWidget {
+  ViewEventsWidget({
+    Key key,
+    this.enventphoto,
+  }) : super(key: key);
+
+  final String enventphoto;
+
+  @override
+  _ViewEventsWidgetState createState() => _ViewEventsWidgetState();
+}
+
+class _ViewEventsWidgetState extends State<ViewEventsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Color(0xFFEEEEEE),
+          ),
+          child: Image.network(
+            'https://picsum.photos/seed/949/600',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 1,
+            fit: BoxFit.cover,
+          ),
+        )
+      ],
+    );
+  }
+}
