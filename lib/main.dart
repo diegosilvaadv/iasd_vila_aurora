31262d119419568c817ff914e3da7f910128a1de
diegosilvaadv <91504617+diegosilvaadv@users.noreply.github.com>
2021-11-16 21:07:42 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
+}
 
-  {
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
