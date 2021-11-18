de7069c54a0e31d94f7765ea61b658465b1a4669
diegosilvaadv <91504617+diegosilvaadv@users.noreply.github.com>
2021-11-17 21:05:22 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,6 @@ void main() async {
   runApp(MyApp());
 }
 
-//Remove this method to stop OneSignal Debugging 
-OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
-
-OneSignal.shared.setAppId("7d11bc10-7cfb-42c6-8315-abdba913a150");
-
-
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
